314982e37136ba6b80c16bb039ec24fd306edeae
casareafer <casareafer@gmail.com>
2014-02-14 15:28:09 -0600

    aa mapas
    aa

--- lander-desktop/src/com/tiarsoft/lander/Main.java
+++ lander-desktop/src/com/tiarsoft/lander/Main.java
@@ -2,9 +2,6 @@ package com.tiarsoft.lander;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
-import com.badlogic.gdx.tools.imagepacker.TexturePacker2.Settings;
 
 public class Main {
 	public static void main(String[] args) {
@@ -14,13 +11,13 @@ public class Main {
 		cfg.width = 480;
 		cfg.height = 800;
 		//
-		Settings set = new Settings();
-		set.maxHeight = 2048;
-		set.maxWidth = 2048;
-		set.filterMag = TextureFilter.Linear;
-		set.filterMin = TextureFilter.Linear;
-		set.flattenPaths = true;
-		set.combineSubdirectories = true;
+//		Settings set = new Settings();
+//		set.maxHeight = 2048;
+//		set.maxWidth = 2048;
+//		set.filterMag = TextureFilter.Linear;
+//		set.filterMin = TextureFilter.Linear;
+//		set.flattenPaths = true;
+//		set.combineSubdirectories = true;
 
 		// TexturePacker2.process(set, "/Users/Yayo/Pictures/Games/Lander/Images", "/Users/Yayo/Documents/eclipseAndroid/Lander/lander-android/assets/data/", "atlasMap.txt");
 
